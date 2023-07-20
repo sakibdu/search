26b15405215f09b9576c866e950a6c5aafcb8875
dev-dhairysheel <103884089+dev-dhairysheel@users.noreply.github.com>
2023-07-20 21:22:39 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,50 @@
+import streamlit as st #line:1
+import time #line:2
+import os #line:3
+st .title ("CleverMate")#line:5
+st .set_page_config (page_title ='CleverMate',page_icon ='🔍')#line:6
+huggingfacehub_api_token =st .secrets ["hf_token"]#line:8
+from langchain import HuggingFaceHub ,PromptTemplate ,LLMChain #line:10
+repo_id ="tiiuae/falcon-7b-instruct"#line:12
+llm =HuggingFaceHub (huggingfacehub_api_token =huggingfacehub_api_token ,repo_id =repo_id ,model_kwargs ={"temperature":0.2 ,"max_new_tokens":2000 })#line:14
+template ="""
+You are an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.
+{question}
+"""#line:19
+prompt =PromptTemplate (template =template ,input_variables =["question"])#line:21
+llm_chain =LLMChain (prompt =prompt ,verbose =True ,llm =llm )#line:23
+def has_num (O00O00OOO0O00OOOO ):#line:25
+    return any (O00O00OOO0O00OOOO .isnumeric ()for O0O0OOO0OOOOOO00O in O00O00OOO0O00OOOO )#line:26
+def chat (O00O00OO000O00OO0 ):#line:28
+    if any (OOO00O0000O0OO000 in O00O00OO000O00OO0 for OOO00O0000O0OO000 in ['+','-','/','*'])and has_num (O00O00OO000O00OO0 ):#line:29
+        return "Apologies, but I am not currently equipped to perform mathematical expressions. My primary function is to provide assistance and information in various areas. If there's anything else you need help with or any other topic you'd like to discuss, please let me know, and I'll be glad to assist you."#line:30
+    OOOOO0OO00OOO000O =llm_chain .predict (question =O00O00OO000O00OO0 )#line:32
+    if any (OO000OO0O0OO00O0O in OOOOO0OO00OOO000O for OO000OO0O0OO00O0O in ['actor','actress','fictional character','Bharatiya Janata Party','Indian National Congress','Indian entrepreneur']):#line:34
+        return "I apologize for the inconvenience. At the moment, I don't have sufficient information to provide a precise answer to your question. Could you kindly provide additional context or details regarding your inquiry? This will enable me to offer more accurate and tailored assistance."#line:35
+    return OOOOO0OO00OOO000O #line:37
+def main ():#line:39
+    O00O00OO0000OOOOO =st .text_input ("What do you want to ask about",placeholder ="Input your question here")#line:40
+    OOOO00OO00O0O000O =O00O00OO0000OOOOO #line:41
+    with st .sidebar :#line:43
+        O0OOO0OO00OOO00O0 =st .sidebar .radio ('Please select mode for the Search',('Brief','Normal','Descriptive'),index =1 )#line:44
+        if O0OOO0OO00OOO00O0 =='Brief':#line:45
+            OOOO00OO00O0O000O =O00O00OO0000OOOOO +", give short answer"#line:46
+        elif O0OOO0OO00OOO00O0 =='Descriptive':#line:47
+            OOOO00OO00O0O000O =O00O00OO0000OOOOO +'. Please explain it in detail, be descriptive'#line:48
+        else :#line:49
+            OOOO00OO00O0O000O =O00O00OO0000OOOOO #line:50
+        O000O0O0OOO00O0O0 =st .checkbox ('Include example')#line:52
+        if O000O0O0OOO00O0O0 :#line:53
+            OOOO00OO00O0O000O =OOOO00OO00O0O000O +'. Give me example'#line:54
+    if O00O00OO0000OOOOO :#line:56
+        OO0OO00OOO0O000O0 =chat (OOOO00OO00O0O000O )#line:57
+        OO0OO00000000000O =OO0OO00OOO0O000O0 #line:58
+        OO0OO00000000000O =OO0OO00000000000O .replace ('<p>','').replace ('</p>','')#line:59
+        OO0OO000000OOOO00 =''#line:60
+        O000O0OO00OO0O0O0 =st .empty ()#line:61
+        for O000OO0OO000OO0O0 in OO0OO00000000000O :#line:62
+            OO0OO000000OOOO00 =OO0OO000000OOOO00 +O000OO0OO000OO0O0 #line:63
+            time .sleep (0.01 )#line:64
+            O000O0OO00OO0O0O0 .write (OO0OO000000OOOO00 )#line:65
+if __name__ =='__main__':#line:67
+    main ()
